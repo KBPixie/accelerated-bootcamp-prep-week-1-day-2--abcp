7b70bbd92e2d1256121d4e0d8fcbe4d4ab15a2fe
Kristen Bullock <kristenbullock84@gmail.com>
2018-01-14 18:58:34 +0000

    automatically backed up by learn

--- if.js
+++ if.js
@@ -4,5 +4,15 @@ if (pokemon == "Pikachu") {
   console.log("I choose you Pikachu!!")
 }
 
+function takeANumber(num) {
+  if (num > 10) {
+    return num;
+  } else {
+    return "sorry, wrong number";
+  }
+}
 
+for (i= 0; i < names.length; i++){
+  console.log
+}
 
